a73c6a9e952ad918b79a3c74885b717b800ce1ca
Piyush Singhal <psi111@sapient.com>
2017-11-05 16:21:53 +0530

    cleaning 2.0

--- AppointmentAnalysis/EDA/EDA.r
+++ AppointmentAnalysis/EDA/EDA.r
@@ -1,6 +1,8 @@
 library(dplyr)
 library(tidyr)
 library(lubridate)
+library(ggplot2)
+library(stringr)
 dataset <- read.csv("KaggleV2-May-2016.csv", stringsAsFactors = F)
 
 nas <- sum(is.na(dataset))
@@ -13,37 +15,76 @@ summary(dataset)
 #Removing unnecessary columns
 dataset$PatientId <- NULL
 dataset$AppointmentID <- NULL
-
-derived_ds <- separate(dataset, ScheduledDay, into=c("Sch.Date", "Sch.Time"), sep="T")
-derived_ds <- separate(derived_ds, AppointmentDay, into=c("App.Date","App.Time"),sep="T")
-derived_ds$App.Time <- NULL
-derived_ds <- separate(derived_ds, Sch.Time, into=c("Sch.Time"),sep="Z")
-
-
-derived_ds$Sch.Date <- parse_date_time(derived_ds$Sch.Date,orders = c("ymd"),locale = "eng")
-derived_ds$Sch.Time <- parse_date_time(derived_ds$Sch.Time,orders=c("HMS"),locale = "eng")
-
-derived_ds$App.Date <- parse_date_time(derived_ds$App.Date,orders=c("ymd"),locale = "eng")
-
-derived_ds$Sch.WeekDay <- weekdays(derived_ds$Sch.Date)
-derived_ds$Sch.Hour <- format(derived_ds$Sch.Time,"%H")
-derived_ds$Sch.Day <- format(derived_ds$Sch.Date,"%d")
+derived_ds <- dataset
+derived_ds$AppointmentDay <- str_replace_all(derived_ds$AppointmentDay,"T"," ")
+derived_ds$AppointmentDay <- str_replace_all(derived_ds$AppointmentDay,"Z","")
+derived_ds$ScheduledDay <- str_replace_all(derived_ds$ScheduledDay,"T"," ")
+derived_ds$ScheduledDay <- str_replace_all(derived_ds$ScheduledDay,"Z","")
 
 
+derived_ds$AppointmentDay <- parse_date_time(derived_ds$AppointmentDay,orders = c("ymd HMS"),locale = "eng")
+
+derived_ds$ScheduledDay <- parse_date_time(derived_ds$ScheduledDay,orders = c("ymd HMS"),locale = "eng")
+
+derived_ds$App.WeekDay <- weekdays(derived_ds$AppointmentDay)
+
+derived_ds$App.Day <- format(derived_ds$AppointmentDay,"%d")
+
+derived_ds$Gap <- round(derived_ds$AppointmentDay - derived_ds$ScheduledDay,0)
+derived_ds$Gap <- ifelse(derived_ds$Gap<0,0,derived_ds$Gap)
+derived_ds$Gap <- round(derived_ds$Gap/24,1)
+
+#Cleaning dirty rows
+
+derived_ds$Age[which(derived_ds$Age==-1)] <- 0
 
 #Factoring categorical variables
 
 derived_ds$Gender <- as.factor(derived_ds$Gender)
 derived_ds$Neighbourhood <- as.factor(derived_ds$Neighbourhood)
-derived_ds$Sch.WeekDay <- as.factor(derived_ds$Sch.WeekDay)
-derived_ds$Sch.Day <- as.factor(derived_ds$Sch.Day)
-derived_ds$Sch.Hour <- as.factor(derived_ds$Sch.Hour)
+derived_ds$App.WeekDay <- as.factor(derived_ds$App.WeekDay)
+derived_ds$App.Day <- as.factor(derived_ds$App.Day)
 
+#Binning Age group
 
 
 derived_ds$No.show <- ifelse(derived_ds$No.show=="Yes",1,0)
+derived_ds$Age.Group <- ''
 
-
+st<-timestamp()
+for(i in 1:nrow(derived_ds)){
+  
+  if(!is.na(derived_ds$Age[i])){
+    if(derived_ds$Age[i] <= 1){
+      derived_ds$Age.Group[i] <- 'Infant'
+    } else if(derived_ds$Age[i] > 1 & derived_ds$Age[i] <=3 &(!is.na(derived_ds$Age[i]))){
+      derived_ds$Age.Group[i] <- 'Toddler'
+    } else if(derived_ds$Age[i] > 3 & derived_ds$Age[i] <=12 &(!is.na(derived_ds$Age[i]))){
+      derived_ds$Age.Group[i] <- 'Pre School'
+    } else if(derived_ds$Age[i] > 12 & derived_ds$Age[i] <= 17&(!is.na(derived_ds$Age[i]))){
+      derived_ds$Age.Group[i] <- 'Grad School'
+    } else if(derived_ds$Age[i] > 17 & derived_ds$Age[i] <= 21&(!is.na(derived_ds$Age[i]))){
+      derived_ds$Age.Group[i] <- 'Young adult'
+    } else if(derived_ds$Age[i] > 21 & derived_ds$Age[i] <= 40&(!is.na(derived_ds$Age[i]))){
+      derived_ds$Age.Group[i] <- 'Adult'
+    } else if(derived_ds$Age[i] > 40 & derived_ds$Age[i] <= 60&(!is.na(derived_ds$Age[i]))){
+      derived_ds$Age.Group[i] <- 'Senior Citizen'
+    } else if(derived_ds$Age[i] > 60){ 
+      derived_ds$Age.Group[i] <- 'Old Citizen' 
+    }
+  } else{
+    
+  }
+}
+et<-timestamp()-st
 
 #Univariate Analysis
+ggplot(derived_ds,aes(x=as.factor(No.show),fill=as.factor(No.show))) + geom_bar()
 
+
+ggplot(derived_ds,aes(x=Age,fill=as.factor(No.show))) + geom_histogram(binwidth = 10)
+
+
+
+
+ggplot(derived_ds,aes(x=as.factor(Age.Group),fill=as.factor(No.show))) + geom_bar() #+ facet_wrap(~Gender) + 
